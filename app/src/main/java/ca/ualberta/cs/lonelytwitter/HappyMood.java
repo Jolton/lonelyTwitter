a62f61c0ac73990762570c6cb6eaac193573a17e
Jolton <chris@MacBook-Pro314.local>
2017-09-12 18:47:17 -0600

    Lab Excercise complete

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/HappyMood.java
@@ -0,0 +1,15 @@
+package ca.ualberta.cs.lonelytwitter;
+
+/**
+ * Created by chris on 2017-09-12.
+ */
+
+public class HappyMood extends Mood {
+
+    public String toString() {
+        return "Happy Mood{" +
+                "date=" + getDate() +
+                '}';
+
+    }
+}
---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/SadMood.java
@@ -0,0 +1,15 @@
+package ca.ualberta.cs.lonelytwitter;
+
+/**
+ * Created by chris on 2017-09-12.
+ */
+
+public class SadMood extends Mood {
+
+    public String toString() {
+        return "Sad Mood{" +
+                "date=" + getDate() +
+                '}';
+
+    }
+}
